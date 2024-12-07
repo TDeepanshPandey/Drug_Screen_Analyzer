58f48841c1e316a784dfe3b5ad067980a0247619
Deepansh Pandey <easypandey.dp@gmail.com>
2024-12-07 08:31:58 +0100

    Rename Tab

--- R/gr_metrics_ui.R
+++ R/gr_metrics_ui.R
@@ -32,7 +32,7 @@ tab1_ui <- function(id) {
     fileInput(ns("file"), "Choose Excel File", multiple = FALSE, accept = c(".xlsx")),
     textInput(ns("output_folder_directory"), "Output Folder Directory", "output"),
     tabsetPanel(
-      tabPanel("Generate Excel Files",
+      tabPanel("Generate Output Excel",
                checkboxInput(ns("save_gr_metrics_xlsx"), "Save GR Metrics Parameter Table (Excel)", value = FALSE),
                checkboxInput(ns("save_gr_values_xlsx"), "Save Original Data with GR Values (Excel)", value = FALSE),
                actionButton(ns("generate_files"), "Generate Files"),
